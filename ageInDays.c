7591d506b599a51b6ec272c230e365b12cea0cce
Julia <jsilvabarb@gmail.com>
2024-10-15 11:24:33 -0300

    feat: add age in days beecrowd problem

---
+++ ageInDays.c
@@ -0,0 +1,18 @@
+int main() {
+ 
+  int ageInDays;
+  
+  scanf("%d", &ageInDays);
+  
+  int ageYears = ageInDays / 365;
+  
+  int restAgeYears = ageInDays % 365;
+  
+  int ageMonths = restAgeYears / 30;
+  
+  int ageDays = restAgeYears % 30;
+  
+  printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ageYears, ageMonths, ageDays);
+
+  return 0;
+}
